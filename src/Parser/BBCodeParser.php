30b857b358346492f5ddc7555fc7f03ac9d06dd6
girni <adrian.affek1@gmail.com>
2018-05-13 01:45:34 +0200

    Added some new parsers

--- src/Parser/BBCodeParser.php
+++ src/Parser/BBCodeParser.php
@@ -36,9 +36,14 @@ final class BBCodeParser extends Parser
             'replace' => '<blockquote>$1</blockquote>',
             'content' => '$1'
         ],
+        'namedquote' => [
+            'pattern' => '/\[quote\=(.*?)\](.*)\[\/quote\]/s',
+            'replace' => '<blockquote><small>$1</small>$2</blockquote>',
+            'content' => '$2',
+        ],
         'link' => [
             'pattern' => '/\[url\](.*?)\[\/url\]/s',
-            'replace' => '<a href="$1">$1</a>',
+            'replace' => '<a rel="nofollow" target="_blank" href="$1">$1</a>',
             'content' => '$1'
         ],
         'namedlink' => [
@@ -52,7 +57,7 @@ final class BBCodeParser extends Parser
             'content' => '$1'
         ],
         'orderedlistnumerical' => [
-            'pattern' => '/\[list=1\](.*?)\[\/list\]/s',
+            'pattern' => '/\[ol\](.*?)\[\/ol\]/s',
             'replace' => '<ol>$1</ol>',
             'content' => '$1'
         ],
@@ -62,12 +67,12 @@ final class BBCodeParser extends Parser
             'content' => '$1'
         ],
         'unorderedlist' => [
-            'pattern' => '/\[list\](.*?)\[\/list\]/s',
+            'pattern' => '/\[ul\](.*?)\[\/ul\]/s',
             'replace' => '<ul>$1</ul>',
             'content' => '$1'
         ],
         'listitem' => [
-            'pattern' => '/\[\*\](.*)/',
+            'pattern' => '/\[li\](.*?)\[\/li\]/s',
             'replace' => '<li>$1</li>',
             'content' => '$1'
         ],
@@ -110,6 +115,31 @@ final class BBCodeParser extends Parser
             'pattern' => '/\[td\](.*?)\[\/td\]/s',
             'replace' => '<td>$1</td>',
             'content' => '$1',
+        ],
+        'color' => [
+            'pattern' => '/\[color\=(#[A-f0-9]{6}|#[A-f0-9]{3})\](.*?)\[\/color\]/s',
+            'replace' => '<span style="color: $1">$2</span>',
+            'content' => '$2',
+        ],
+        'center' => [
+            'pattern' => '/\[center\](.*?)\[\/center\]/s',
+            'replace' => '<div style="text-align:center;">$1</div>',
+            'content' => '$1',
+        ],
+        'left' => [
+            'pattern' => '/\[left\](.*?)\[\/left\]/s',
+            'replace' => '<div style="text-align:left;">$1</div>',
+            'content' => '$1',
+        ],
+        'right' => [
+            'pattern' => '/\[right\](.*?)\[\/right\]/s',
+            'replace' => '<div style="text-align:right;">$1</div>',
+            'content' => '$1',
+        ],
+        'size' => [
+            'pattern' => '/\[size=(.+)](.+)\[/size\]/Usi',
+            'replace' => '<span class="font-size-$1">$2</span>',
+            'content' => '$2',
         ],
     ];
 
